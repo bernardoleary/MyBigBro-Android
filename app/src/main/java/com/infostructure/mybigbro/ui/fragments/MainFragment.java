d4d3acffa32f0d91873ff5e9e1d23c399f946361
Bernard <bernard.oleary@gmail.com>
2015-04-19 10:36:15 +1200

    Made some UI/UX changes to the front screen and changed some wording in Settings.

--- app/src/main/java/com/infostructure/mybigbro/ui/fragments/MainFragment.java
+++ app/src/main/java/com/infostructure/mybigbro/ui/fragments/MainFragment.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,7 @@ import com.infostructure.mybigbro.R;
 import com.infostructure.mybigbro.services.DataAccessService;
 import com.infostructure.mybigbro.services.GeoMarkerService;
 import com.infostructure.mybigbro.ui.OnFragmentInteractionListener;
+import com.infostructure.mybigbro.ui.activities.SettingsActivity;
 
 public class MainFragment extends Fragment {
 
@@ -34,11 +37,17 @@ public class MainFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
     private int mSectionNumber;
     private OnFragmentInteractionListener mListener;
-    private Switch mSwitchGeoMarkerServiceIsRunning;
     private LocationManager mLocationManager;
+    ToggleButton mToggleButton;
+    TextView mTextViewMessageRunning1;
+    TextView mTextViewMessageRunning2;
+    TextView mTextViewMessagePaused1;
+    TextView mTextViewMessagePaused2;
+    TextView mTextViewMessageRunningDeviceName;
+    TextView mTextViewMessagePausedDeviceName;
 
-    ToggleButton toggleButton;
-    TextView text;
+    // Services
+    private DataAccessService mDataAccessService;
 
     public static MainFragment newInstance(int sectionNumber) {
         MainFragment fragment = new MainFragment();
@@ -79,6 +88,10 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        /* Create the services that we need */
+        this.mDataAccessService = DataAccessService.getInstance();
+        this.mDataAccessService.setApplicationContext(this.getActivity().getApplicationContext());
+
         /* GPS Enabled check */
         this.mLocationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
         if (!this.mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
@@ -91,50 +104,62 @@ public class MainFragment extends Fragment {
             }, 2000); // starting it in 2 seconds...
         }
 
-        toggleButton = (ToggleButton)rootView.findViewById(R.id.toggleButton);
-        toggleButton.setChecked(isGeoMarkerServiceRunning());
+        this.mTextViewMessageRunning1 = (TextView)rootView.findViewById(R.id.textViewMessageRunning1);
+        this.mTextViewMessageRunning2 = (TextView)rootView.findViewById(R.id.textViewMessageRunning2);
+        this.mTextViewMessagePaused1 = (TextView)rootView.findViewById(R.id.textViewMessagePaused1);
+        this.mTextViewMessagePaused2 = (TextView)rootView.findViewById(R.id.textViewMessagePaused2);
+        this.mTextViewMessageRunningDeviceName = (TextView)rootView.findViewById(R.id.textViewMessageRunningDeviceName);
+        this.mTextViewMessagePausedDeviceName = (TextView)rootView.findViewById(R.id.textViewMessagePausedDeviceName);
+        this.mToggleButton = (ToggleButton)rootView.findViewById(R.id.toggleButton);
+        this.mToggleButton.setChecked(isGeoMarkerServiceRunning());
         //text = (TextView)rootView.findViewById(R.id.textView1);
         //text.setText("Status: " + isGeoMarkerServiceRunning());
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        this.mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton arg0, boolean isChecked) {
                 if (isChecked) {
                     if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                         getActivity().startService(new Intent(getActivity(), GeoMarkerService.class));
                     } else {
-                        toggleButton.setChecked(false);
+                        mToggleButton.setChecked(false);
                         showGPSDisabledAlertToUser();
                     }
                 } else {
                     getActivity().stopService(new Intent(getActivity(), GeoMarkerService.class));
                 }
-                //text.setText("Status: " + toggleButton.isChecked());
+                setTextViewRunningOrPaused();
             }
         });
 
-        /* Set up UI */
-        /*
-        this.mSwitchGeoMarkerServiceIsRunning = (Switch)rootView.findViewById(R.id.switchGeoMarkerServiceIsRunning);
-        this.mSwitchGeoMarkerServiceIsRunning.setChecked(isGeoMarkerServiceRunning());
-        this.mSwitchGeoMarkerServiceIsRunning.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                        getActivity().startService(new Intent(getActivity(), GeoMarkerService.class));
-                    } else {
-                        mSwitchGeoMarkerServiceIsRunning.setChecked(false);
-                        showGPSDisabledAlertToUser();
-                    }
-                } else {
-                    getActivity().stopService(new Intent(getActivity(), GeoMarkerService.class));
-                }
-            }
-        });
-        */
+        /* Set message visiblity */
+        this.setTextViewRunningOrPaused();
 
         /* Return the view */
         return rootView;
+    }
+
+    private void setTextViewRunningOrPaused() {
+        boolean isRunning = this.mToggleButton.isChecked();
+        this.mTextViewMessageRunning1.setVisibility(isRunning ? View.VISIBLE : View.GONE);
+        this.mTextViewMessageRunning2.setVisibility(isRunning ? View.VISIBLE : View.GONE);
+        this.mTextViewMessageRunningDeviceName.setVisibility(isRunning ? View.VISIBLE : View.GONE);
+        this.mTextViewMessagePaused1.setVisibility(isRunning ? View.GONE : View.VISIBLE);
+        this.mTextViewMessagePaused2.setVisibility(isRunning ? View.GONE : View.VISIBLE);
+        this.mTextViewMessagePausedDeviceName.setVisibility(isRunning ? View.GONE : View.VISIBLE);
+        try {
+            this.mTextViewMessagePausedDeviceName.setText("Your device name is: '" + this.mDataAccessService.getDeviceName() + "'\nClick here to change.");
+            this.mTextViewMessagePausedDeviceName.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v){
+                    Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                    startActivity(intent);
+                }
+            });
+            this.mTextViewMessageRunningDeviceName.setText(Html.fromHtml("Browse to: <a href=\"http://mybigbro.tv/?devicename=" + this.mDataAccessService.getDeviceName() + "\">mybigbro.tv/?devicename=" + this.mDataAccessService.getDeviceName() + "</a>\nto see your captured images!"));
+            this.mTextViewMessageRunningDeviceName.setLinksClickable(true);
+            this.mTextViewMessageRunningDeviceName.setMovementMethod(LinkMovementMethod.getInstance());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void showGPSDisabledAlertToUser() {
